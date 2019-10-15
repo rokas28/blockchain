25f80f5ee986f4d9604bb4a2601a9ce8168f515a
RokasT <rokas.tamulevicius28@gmail.com>
2019-10-16 00:42:31 +0300

    update

--- user.h
+++ user.h
@@ -5,10 +5,24 @@
 
 class User {
 private:
-    string name;
-    string publicKey;
-    double balance;
+    string name_;
+    string publicKey_;
+    double balance_;
 public:
+    User(string name, string publicKey, double balance);
+    User();
+};
+
+User::User(string name, string publicKey, double balance){
+    this -> name_ = name;
+    this -> publicKey_ = publicKey;
+    this -> balance_ = balance;
+};
+
+User::User(){
+    this -> name_ = "-";
+    this -> publicKey_ = "-";
+    this -> balance_ = 0;
 };
 
 #endif //BLOCKCHAIN_USER_H
