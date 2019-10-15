bbc6210ac7810b284501e1604fd9aedd21ee634b
RokasT <rokas.tamulevicius28@gmail.com>
2019-10-15 20:24:57 +0300

    update

--- block.h
+++ block.h
@@ -1,4 +1,18 @@
 #ifndef BLOCKCHAIN_BLOCK_H
 #define BLOCKCHAIN_BLOCK_H
 
+# include "main.h"
+
+class Block{
+private:
+    int index;
+    int nonce;
+    string prevHash;
+    string blockHash;
+    string merkelRootHash;
+    time_t timestamp;
+    int difficulty;
+    vector<string> transactions;
+public:
+};
 #endif //BLOCKCHAIN_BLOCK_H
---
+++ blockchain.h
@@ -0,0 +1,13 @@
+#ifndef BLOCKCHAIN_BLOCKCHAIN_H
+#define BLOCKCHAIN_BLOCKCHAIN_H
+
+#include "main.h"
+#include "block.h"
+
+class Blockchain {
+private:
+    vector<Block> chain;
+public:
+};
+
+#endif //BLOCKCHAIN_BLOCKCHAIN_H
