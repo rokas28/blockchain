50aa4a940934324449ede9d23050b91fe39c9ec8
RokasT <rokas.tamulevicius28@gmail.com>
2019-10-16 00:24:57 +0300

    user

---
+++ user.h
@@ -0,0 +1,14 @@
+#ifndef BLOCKCHAIN_USER_H
+#define BLOCKCHAIN_USER_H
+
+#include "main.h"
+
+class User {
+private:
+    string name;
+    string publicKey;
+    double balance;
+public:
+};
+
+#endif //BLOCKCHAIN_USER_H
